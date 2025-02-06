70c9bce942f9fd6f35d0f3b0439ae574987e223c
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-02-07 00:25:39 +0200

    Stateless

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header.jsx';
+import './App.css';
+
+function App() {
+  const component = 'Stateless';
+  return (
+    <>
+      <Header type={component}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export function Header({type}) {
+    return (
+        <>
+            <h1>Приклад коду, {type} компонент (без стану)</h1>
+        </>
+    )
+
+}
+
+export default Header
